da11c14684ee126ca8c8c6e90e69289de6040814
joeycastillo <joeycastillo@utexas.edu>
2024-09-18 17:13:11 -0400

    use gossamer GPIO calls here

--- watch-library/hardware/watch/watch_private.c
+++ watch-library/hardware/watch/watch_private.c
@@ -32,9 +32,8 @@ void _watch_init(void) {
     watch_disable_digital_output(GPIO(GPIO_PORTA, 20));
 
     // disable debugger hot-plugging
-    gpio_set_pin_function(SWCLK, GPIO_PIN_FUNCTION_OFF);
-    gpio_set_pin_direction(SWCLK, GPIO_DIRECTION_OFF);
-    gpio_set_pin_pull_mode(SWCLK, GPIO_PULL_OFF);
+    HAL_GPIO_SWCLK_pmuxdis();
+    HAL_GPIO_SWCLK_off();
 
     // RAM should be back-biased in STANDBY
     PM->STDBYCFG.bit.BBIASHS = 1;
