voloved/Sensor-Watch-Fork

Began removing calls to watch_rtc_get_date_time

movement/watch_faces/clock/day_night_percentage_face.c
@@ -62,7 +62,7 @@ void day_night_percentage_face_setup(movement_settings_t *settings, uint8_t watc
     if (*context_ptr == NULL) {
         *context_ptr = malloc(sizeof(day_night_percentage_state_t));
         day_night_percentage_state_t *state = (day_night_percentage_state_t *)*context_ptr;
-        watch_date_time utc_now = watch_utility_date_time_convert_zone(watch_rtc_get_date_time(), movement_get_current_timezone_offset(), 0);
+        watch_date_time utc_now = movement_get_utc_date_time();
         recalculate(utc_now, state);
     }
 }
@@ -76,8 +76,8 @@ bool day_night_percentage_face_loop(movement_event_t event, movement_settings_t 
     day_night_percentage_state_t *state = (day_night_percentage_state_t *)context;
 
     char buf[12];
-    watch_date_time date_time = watch_rtc_get_date_time();
-    watch_date_time utc_now = watch_utility_date_time_convert_zone(date_time, movement_get_current_timezone_offset(), 0);
+    watch_date_time utc_now = movement_get_utc_date_time();
+    watch_date_time date_time = watch_utility_date_time_convert_zone(utc_now, 0, movement_get_current_timezone_offset());
 
     switch (event.event_type) {
         case EVENT_ACTIVATE:

movement/watch_faces/complication/activity_face.c
@@ -199,7 +199,7 @@ static void _activity_activate(movement_settings_t *settings, activity_state_t *
     // Those are not up-to-date because ticks have not been coming
     if (state->le_state != 0 && state->mode == ACTM_LOGGING) {
         state->le_state = 2;
-        watch_date_time now = watch_rtc_get_date_time();
+        watch_date_time now = movement_get_local_date_time();
         uint32_t now_timestamp = watch_utility_date_time_to_unix_time(now, 0);
         uint32_t start_timestamp = watch_utility_date_time_to_unix_time(state->start_time, 0);
         uint32_t total_seconds = now_timestamp - start_timestamp;
@@ -246,7 +246,7 @@ static void _activity_update_logging_screen(movement_settings_t *settings, activ
 
     // If we're in LE state: per-minute update is special
     if (state->le_state == 1) {
-        watch_date_time now = watch_rtc_get_date_time();
+        watch_date_time now = movement_get_local_date_time();
         uint32_t now_timestamp = watch_utility_date_time_to_unix_time(now, 0);
         uint32_t start_timestamp = watch_utility_date_time_to_unix_time(state->start_time, 0);
         uint32_t total_seconds = now_timestamp - start_timestamp;
@@ -294,7 +294,7 @@ static void _activity_update_logging_screen(movement_settings_t *settings, activ
     // Briefly, show time without seconds
     else {
         watch_clear_indicator(WATCH_INDICATOR_LAP);
-        watch_date_time now = watch_rtc_get_date_time();
+        watch_date_time now = movement_get_local_date_time();
         uint8_t hour = now.unit.hour;
         if (!settings->bit.clock_mode_24h) {
             watch_clear_indicator(WATCH_INDICATOR_24H);
@@ -543,7 +543,7 @@ static void _activity_alarm_long(movement_settings_t *settings, activity_state_t
         if (activity_log_count >= ACTIVITY_LOG_SZ)
             return;
         // OK, we go ahead and start logging
-        state->start_time = watch_rtc_get_date_time();
+        state->start_time = movement_get_local_date_time();
         state->curr_total_sec = 0;
         state->curr_pause_sec = 0;
         state->counter = -1;

movement/watch_faces/complication/astronomy_face.c
@@ -79,9 +79,7 @@ static void _astronomy_face_recalculate(astronomy_state_t *state) {
     }
 #endif
 
-    watch_date_time date_time = watch_rtc_get_date_time();
-    uint32_t timestamp = watch_utility_date_time_to_unix_time(date_time, movement_get_current_timezone_offset());
-    date_time = watch_utility_date_time_from_unix_time(timestamp, 0);
+    watch_date_time date_time = movement_get_utc_date_time();
     double jd = astro_convert_date_to_julian_date(date_time.unit.year + WATCH_RTC_REFERENCE_YEAR, date_time.unit.month, date_time.unit.day, date_time.unit.hour, date_time.unit.minute, date_time.unit.second);
 
     astro_equatorial_coordinates_t radec_precession = astro_get_ra_dec(jd, astronomy_available_celestial_bodies[state->active_body_index], state->latitude_radians, state->longitude_radians, true);

movement/watch_faces/complication/moon_phase_face.c
@@ -58,8 +58,8 @@ void moon_phase_face_activate(movement_settings_t *settings, void *context) {
 static void _update(moon_phase_state_t *state, uint32_t offset) {
     (void)state;
     char buf[11];
-    watch_date_time date_time = watch_rtc_get_date_time();
-    uint32_t now = watch_utility_date_time_to_unix_time(date_time, movement_get_current_timezone_offset()) + offset;
+    watch_date_time date_time = movement_get_utc_date_time();
+    uint32_t now = watch_utility_date_time_to_unix_time(date_time, 0) + offset;
     date_time = watch_utility_date_time_from_unix_time(now, movement_get_current_timezone_offset());
     double currentfrac = fmod(now - FIRST_MOON, LUNAR_SECONDS) / LUNAR_SECONDS;
     double currentday = currentfrac * LUNAR_DAYS;

movement/watch_faces/complication/ships_bell_face.c
@@ -28,7 +28,7 @@
 #include "ships_bell_face.h"
 
 static void ships_bell_ring() {
-    watch_date_time date_time = watch_rtc_get_date_time();
+    watch_date_time date_time = movement_get_local_date_time();
 
     date_time.unit.hour %= 4;
     date_time.unit.hour = date_time.unit.hour == 0 && date_time.unit.minute < 30 ? 4 : date_time.unit.hour;
@@ -54,7 +54,7 @@ static void ships_bell_draw(ships_bell_state_t *state) {
         sprintf(buf, " ");
     }
 
-    watch_date_time date_time = watch_rtc_get_date_time();
+    watch_date_time date_time = movement_get_local_date_time();
     date_time.unit.hour %= 4;
 
     sprintf(buf + 1, " %d%02d%02d", date_time.unit.hour, date_time.unit.minute, date_time.unit.second);
@@ -134,7 +134,7 @@ bool ships_bell_face_wants_background_task(movement_settings_t *settings, void *
     ships_bell_state_t *state = (ships_bell_state_t *) context;
     if (!state->bell_enabled) return false;
 
-    watch_date_time date_time = watch_rtc_get_date_time();
+    watch_date_time date_time = movement_get_local_date_time();
     if (!(date_time.unit.minute == 0 || date_time.unit.minute == 30)) return false;
 
     date_time.unit.hour %= 12;

movement/watch_faces/complication/solstice_face.c
@@ -136,7 +136,7 @@ void solstice_face_setup(movement_settings_t *settings, uint8_t watch_face_index
         *context_ptr = malloc(sizeof(solstice_state_t));
         solstice_state_t *state = (solstice_state_t *)*context_ptr;
 
-        watch_date_time now = watch_rtc_get_date_time();
+        watch_date_time now = movement_get_local_date_time();
         state->year = now.unit.year;
         state->index = 0;
         calculate_datetimes(state);

joeycastillo/second-movement

movement/watch_faces/clock/day_night_percentage_face.c
@@ -61,7 +61,7 @@ void day_night_percentage_face_setup(uint8_t watch_face_index, void ** context_p
     if (*context_ptr == NULL) {
         *context_ptr = malloc(sizeof(day_night_percentage_state_t));
         day_night_percentage_state_t *state = (day_night_percentage_state_t *)*context_ptr;
-        watch_date_time_t utc_now = watch_utility_date_time_convert_zone(watch_rtc_get_date_time(), movement_get_current_timezone_offset(), 0);
+        watch_date_time_t utc_now = movement_get_utc_date_time();
         recalculate(utc_now, state);
     }
 }
@@ -74,8 +74,8 @@ bool day_night_percentage_face_loop(movement_event_t event, void *context) {
     day_night_percentage_state_t *state = (day_night_percentage_state_t *)context;
 
     char buf[12];
-    watch_date_time_t date_time = watch_rtc_get_date_time();
-    watch_date_time_t utc_now = watch_utility_date_time_convert_zone(date_time, movement_get_current_timezone_offset(), 0);
+    watch_date_time_t utc_now = movement_get_utc_date_time();
+    watch_date_time_t date_time = watch_utility_date_time_convert_zone(utc_now, 0, movement_get_current_timezone_offset());
 
     switch (event.event_type) {
         case EVENT_ACTIVATE:

movement/watch_faces/complication/activity_face.c
@@ -197,7 +197,7 @@ static void _activity_activate(activity_state_t *state) {
     // Those are not up-to-date because ticks have not been coming
     if (state->le_state != 0 && state->mode == ACTM_LOGGING) {
         state->le_state = 2;
-        watch_date_time_t now = watch_rtc_get_date_time();
+        watch_date_time_t now = movement_get_local_date_time();
         uint32_t now_timestamp = watch_utility_date_time_to_unix_time(now, 0);
         uint32_t start_timestamp = watch_utility_date_time_to_unix_time(state->start_time, 0);
         uint32_t total_seconds = now_timestamp - start_timestamp;
@@ -244,7 +244,7 @@ static void _activity_update_logging_screen(activity_state_t *state) {
 
     // If we're in LE state: per-minute update is special
     if (state->le_state == 1) {
-        watch_date_time_t now = watch_rtc_get_date_time();
+        watch_date_time_t now = movement_get_local_date_time();
         uint32_t now_timestamp = watch_utility_date_time_to_unix_time(now, 0);
         uint32_t start_timestamp = watch_utility_date_time_to_unix_time(state->start_time, 0);
         uint32_t total_seconds = now_timestamp - start_timestamp;
@@ -292,7 +292,7 @@ static void _activity_update_logging_screen(activity_state_t *state) {
     // Briefly, show time without seconds
     else {
         watch_clear_indicator(WATCH_INDICATOR_LAP);
-        watch_date_time_t now = watch_rtc_get_date_time();
+        watch_date_time_t now = movement_get_local_date_time();
         uint8_t hour = now.unit.hour;
         if (!movement_clock_mode_24h()) {
             watch_clear_indicator(WATCH_INDICATOR_24H);
@@ -541,7 +541,7 @@ static void _activity_alarm_long(activity_state_t *state) {
         if (activity_log_count >= ACTIVITY_LOG_SZ)
             return;
         // OK, we go ahead and start logging
-        state->start_time = watch_rtc_get_date_time();
+        state->start_time = movement_get_local_date_time();
         state->curr_total_sec = 0;
         state->curr_pause_sec = 0;
         state->counter = -1;

movement/watch_faces/complication/astronomy_face.c
@@ -79,9 +79,7 @@ static void _astronomy_face_recalculate(astronomy_state_t *state) {
     }
 #endif
 
-    watch_date_time_t date_time = watch_rtc_get_date_time();
-    uint32_t timestamp = watch_utility_date_time_to_unix_time(date_time, movement_get_current_timezone_offset());
-    date_time = watch_utility_date_time_from_unix_time(timestamp, 0);
+    watch_date_time_t date_time = movement_get_utc_date_time();
     double jd = astro_convert_date_to_julian_date(date_time.unit.year + WATCH_RTC_REFERENCE_YEAR, date_time.unit.month, date_time.unit.day, date_time.unit.hour, date_time.unit.minute, date_time.unit.second);
 
     astro_equatorial_coordinates_t radec_precession = astro_get_ra_dec(jd, astronomy_available_celestial_bodies[state->active_body_index], state->latitude_radians, state->longitude_radians, true);

movement/watch_faces/complication/moon_phase_face.c
@@ -56,8 +56,8 @@ void moon_phase_face_activate(void *context) {
 static void _update(moon_phase_state_t *state, uint32_t offset) {
     (void)state;
     char buf[11];
-    watch_date_time_t date_time = watch_rtc_get_date_time();
-    uint32_t now = watch_utility_date_time_to_unix_time(date_time, movement_get_current_timezone_offset()) + offset;
+    watch_date_time_t date_time = movement_get_utc_date_time();
+    uint32_t now = watch_utility_date_time_to_unix_time(date_time, 0) + offset;
     date_time = watch_utility_date_time_from_unix_time(now, movement_get_current_timezone_offset());
     double currentfrac = fmod(now - FIRST_MOON, LUNAR_SECONDS) / LUNAR_SECONDS;
     double currentday = currentfrac * LUNAR_DAYS;

movement/watch_faces/complication/ships_bell_face.c
@@ -28,7 +28,7 @@
 #include "ships_bell_face.h"
 
 static void ships_bell_ring() {
-    watch_date_time_t date_time = watch_rtc_get_date_time();
+    watch_date_time_t date_time = movement_get_local_date_time();
 
     date_time.unit.hour %= 4;
     date_time.unit.hour = date_time.unit.hour == 0 && date_time.unit.minute < 30 ? 4 : date_time.unit.hour;
@@ -54,7 +54,7 @@ static void ships_bell_draw(ships_bell_state_t *state) {
         sprintf(buf, " ");
     }
 
-    watch_date_time_t date_time = watch_rtc_get_date_time();
+    watch_date_time_t date_time = movement_get_local_date_time();
     date_time.unit.hour %= 4;
 
     sprintf(buf + 1, " %d%02d%02d", date_time.unit.hour, date_time.unit.minute, date_time.unit.second);
@@ -130,8 +130,8 @@ movement_watch_face_advisory_t ships_bell_face_advise(void *context) {
 
     if (!state->bell_enabled) return retval;
 
-    watch_date_time_t date_time = watch_rtc_get_date_time();
-    if (!(date_time.unit.minute == 0 || date_time.unit.minute == 30)) return retval;
+    watch_date_time_t date_time = movement_get_local_date_time();
+    if (!(date_time.unit.minute == 0 || date_time.unit.minute == 30)) return false;
 
     date_time.unit.hour %= 12;
     // #SecondMovement: This was migrated to the new advisory API but not tested. Needs more testing!

movement/watch_faces/complication/solstice_face.c
@@ -135,7 +135,7 @@ void solstice_face_setup(uint8_t watch_face_index, void ** context_ptr) {
         *context_ptr = malloc(sizeof(solstice_state_t));
         solstice_state_t *state = (solstice_state_t *)*context_ptr;
 
-        watch_date_time_t now = watch_rtc_get_date_time();
+        watch_date_time_t now = movement_get_local_date_time();
         state->year = now.unit.year;
         state->index = 0;
         calculate_datetimes(state, settings);