voloved/Sensor-Watch-Fork

continued removing calls to watch_rtc_get_date_time

movement/watch_faces/complication/tachymeter_face.c
@@ -173,7 +173,7 @@ bool tachymeter_face_loop(movement_event_t event, movement_settings_t *settings,
                 if (!state->editing) {
                     // Start running
                     state->running = true;
-                    state->start_seconds = watch_rtc_get_date_time();
+                    state->start_seconds = movement_get_utc_date_time();
                     state->start_subsecond = event.subsecond;
                     state->total_time = 0;
                 } else {
@@ -205,7 +205,7 @@ bool tachymeter_face_loop(movement_event_t event, movement_settings_t *settings,
                 }
                 // Stop running
                 state->running = false;
-                watch_date_time now = watch_rtc_get_date_time();
+                watch_date_time now = movement_get_utc_date_time();
                 uint32_t now_timestamp = watch_utility_date_time_to_unix_time(now, 0);
                 uint32_t start_timestamp = watch_utility_date_time_to_unix_time(state->start_seconds, 0);
                 // Total time in centiseconds

movement/watch_faces/complication/timer_face.c
@@ -36,10 +36,6 @@ static const int8_t _sound_seq_start[] = {BUZZER_NOTE_C8, 2, 0};
 
 static uint8_t _beeps_to_play;    // temporary counter for ring signals playing
 
-static inline int32_t _get_tz_offset(void) {
-    return movement_get_current_timezone_offset();
-}
-
 static void _signal_callback() {
     if (_beeps_to_play) {
         _beeps_to_play--;
@@ -49,8 +45,8 @@ static void _signal_callback() {
 
 static void _start(timer_state_t *state, bool with_beep) {
     if (state->timers[state->current_timer].value == 0) return;
-    watch_date_time now = watch_rtc_get_date_time();
-    state->now_ts = watch_utility_date_time_to_unix_time(now, _get_tz_offset());
+    watch_date_time now = movement_get_utc_date_time();
+    state->now_ts = watch_utility_date_time_to_unix_time(now, 0);
     if (state->mode == pausing)
         state->target_ts = state->now_ts + state->paused_left;
     else
@@ -58,7 +54,7 @@ static void _start(timer_state_t *state, bool with_beep) {
                                                           state->timers[state->current_timer].unit.hours, 
                                                           state->timers[state->current_timer].unit.minutes, 
                                                           state->timers[state->current_timer].unit.seconds);
-    watch_date_time target_dt = watch_utility_date_time_from_unix_time(state->target_ts, _get_tz_offset());
+    watch_date_time target_dt = watch_utility_date_time_from_unix_time(state->target_ts, 0);
     state->mode = running;
     movement_schedule_background_task_for_face(state->watch_face_index, target_dt);
     watch_set_indicator(WATCH_INDICATOR_BELL);
@@ -209,8 +205,8 @@ void timer_face_activate(movement_settings_t *settings, void *context) {
     watch_display_string("TR", 0);
     watch_set_colon();
     if(state->mode == running) {
-        watch_date_time now = watch_rtc_get_date_time();
-        state->now_ts = watch_utility_date_time_to_unix_time(now, _get_tz_offset());
+        watch_date_time now = movement_get_utc_date_time();
+        state->now_ts = watch_utility_date_time_to_unix_time(now, 0);
         watch_set_indicator(WATCH_INDICATOR_BELL);
     } else {
         state->pausing_seconds = 1;

movement/watch_faces/complication/tomato_face.c
@@ -30,10 +30,6 @@
 static uint8_t focus_min = 25;
 static uint8_t break_min = 5;
 
-static inline int32_t get_tz_offset(void) {
-    return movement_get_current_timezone_offset();
-}
-
 static uint8_t get_length(tomato_state_t *state) {
     uint8_t length;
     if (state->kind == tomato_focus) {
@@ -46,13 +42,13 @@ static uint8_t get_length(tomato_state_t *state) {
 }
 
 static void tomato_start(tomato_state_t *state) {
-    watch_date_time now = watch_rtc_get_date_time();
+    watch_date_time now = movement_get_utc_date_time();
     int8_t length = (int8_t) get_length(state);
 
     state->mode = tomato_run;
-    state->now_ts = watch_utility_date_time_to_unix_time(now, get_tz_offset());
+    state->now_ts = watch_utility_date_time_to_unix_time(now, 0);
     state->target_ts = watch_utility_offset_timestamp(state->now_ts, 0, length, 0);
-    watch_date_time target_dt = watch_utility_date_time_from_unix_time(state->target_ts, get_tz_offset());
+    watch_date_time target_dt = watch_utility_date_time_from_unix_time(state->target_ts, 0);
     movement_schedule_background_task(target_dt);
     watch_set_indicator(WATCH_INDICATOR_BELL);
 }
@@ -126,8 +122,8 @@ void tomato_face_activate(movement_settings_t *settings, void *context) {
     (void)settings;
     tomato_state_t *state = (tomato_state_t *)context;
     if (state->mode == tomato_run) {
-        watch_date_time now = watch_rtc_get_date_time();
-        state->now_ts = watch_utility_date_time_to_unix_time(now, get_tz_offset());
+        watch_date_time now = movement_get_utc_date_time();
+        state->now_ts = watch_utility_date_time_to_unix_time(now, 0);
         watch_set_indicator(WATCH_INDICATOR_BELL);
     }
     watch_set_colon();

movement/watch_faces/sensor/accelerometer_data_acquisition_face.c
@@ -441,8 +441,8 @@ static void start_reading(accelerometer_data_acquisition_state_t *state) {
     lis2dw_enable_fifo();
 
     accelerometer_data_acquisition_record_t record;
-    watch_date_time date_time = watch_rtc_get_date_time();
-    state->starting_timestamp = watch_utility_date_time_to_unix_time(date_time, movement_get_current_timezone_offset());
+    watch_date_time date_time = movement_get_utc_date_time();
+    state->starting_timestamp = watch_utility_date_time_to_unix_time(date_time, 0);
     record.header.info.record_type = ACCELEROMETER_DATA_ACQUISITION_HEADER;
     record.header.info.range = ACCELEROMETER_RANGE;
     record.header.info.temperature = lis2dw_get_temperature();

movement/watch_faces/settings/finetune_face.c
@@ -52,7 +52,7 @@ void finetune_face_activate(movement_settings_t *settings, void *context) {
 }
 
 static float finetune_get_hours_passed(void) {
-    uint32_t current_time = watch_utility_date_time_to_unix_time(watch_rtc_get_date_time(), 0);
+    uint32_t current_time = watch_utility_date_time_to_unix_time(movement_get_utc_date_time(), 0);
     return (current_time - nanosec_state.last_correction_time) / 3600.0f;
 }
 
@@ -65,7 +65,7 @@ static void finetune_update_display(void) {
 
     if (finetune_page == 0) {
         watch_display_string("FT", 0);
-        watch_date_time date_time = watch_rtc_get_date_time();
+        watch_date_time date_time = movement_get_utc_date_time();
         sprintf(buf, "%02d", date_time.unit.second);
         watch_display_string(buf, 8);
 
@@ -106,7 +106,7 @@ static void finetune_adjust_subseconds(int delta) {
     watch_rtc_enable(false);
     delay_ms(delta);
     if (delta > 500) {
-        watch_date_time date_time = watch_rtc_get_date_time();
+        watch_date_time date_time = movement_get_local_date_time();
         date_time.unit.second = (date_time.unit.second + 1) % 60;
         if (date_time.unit.second == 0) { // Overflow
             date_time.unit.minute = (date_time.unit.minute + 1) % 60;
@@ -126,7 +126,7 @@ static void finetune_update_correction_time(void) {
     nanosec_state.freq_correction += roundf(nanosec_get_aging() * 100);
 
     // Remember when we last corrected time
-    nanosec_state.last_correction_time = watch_utility_date_time_to_unix_time(watch_rtc_get_date_time(), 0);
+    nanosec_state.last_correction_time = watch_utility_date_time_to_unix_time(movement_get_local_date_time(), 0);
     nanosec_save();
     movement_move_to_face(0); // Go to main face after saving settings
 }
@@ -147,7 +147,7 @@ bool finetune_face_loop(movement_event_t event, movement_settings_t *settings, v
             // We flash green LED once per minute to measure clock error, when we are not on first screen
             if (finetune_page!=0) {
                 watch_date_time date_time;
-                date_time = watch_rtc_get_date_time();
+                date_time = movement_get_utc_date_time();
                 if (date_time.unit.second == 0) {
                     watch_set_led_green();
                     #ifndef __EMSCRIPTEN__

movement/watch_faces/settings/nanosec_face.c
@@ -45,7 +45,7 @@ const float voltage_coefficient = 0.241666667 * dithering; // 10 * ppm/V. Nomina
 static void nanosec_init_profile(void) {
     nanosec_changed = true;
     nanosec_state.correction_cadence = 10;
-    watch_date_time date_time = watch_rtc_get_date_time();
+    watch_date_time date_time = movement_get_utc_date_time();
     nanosec_state.last_correction_time = watch_utility_date_time_to_unix_time(date_time, 0);
 
     // init data after changing profile - do that once per profile selection
@@ -261,7 +261,7 @@ static void nanosec_next_edit_screen(void) {
 
 float nanosec_get_aging() // Returns aging correction in ppm
 {
-    watch_date_time date_time = watch_rtc_get_date_time();
+    watch_date_time date_time = movement_get_utc_date_time();
     float years = (watch_utility_date_time_to_unix_time(date_time, 0) - nanosec_state.last_correction_time) / 31536000.0f; // Years passed since finetune
     return years*nanosec_state.aging_ppm_pa/100.0f;
 }
@@ -370,7 +370,7 @@ bool nanosec_face_wants_background_task(movement_settings_t *settings, void *con
     (void) context;
     if (nanosec_state.correction_profile == 0)
         return 0; // No need for background correction if we are on profile 0 - static hardware correction.
-    watch_date_time date_time = watch_rtc_get_date_time();
+    watch_date_time date_time = movement_get_utc_date_time();
 
     return date_time.unit.minute % nanosec_state.correction_cadence == 0;
 }

joeycastillo/second-movement

movement/watch_faces/complication/tachymeter_face.c
@@ -170,7 +170,7 @@ bool tachymeter_face_loop(movement_event_t event, void *context) {
                 if (!state->editing) {
                     // Start running
                     state->running = true;
-                    state->start_seconds = watch_rtc_get_date_time();
+                    state->start_seconds = movement_get_utc_date_time();
                     state->start_subsecond = event.subsecond;
                     state->total_time = 0;
                 } else {
@@ -202,7 +202,7 @@ bool tachymeter_face_loop(movement_event_t event, void *context) {
                 }
                 // Stop running
                 state->running = false;
-                watch_date_time_t now = watch_rtc_get_date_time();
+                watch_date_time_t now = movement_get_utc_date_time();
                 uint32_t now_timestamp = watch_utility_date_time_to_unix_time(now, 0);
                 uint32_t start_timestamp = watch_utility_date_time_to_unix_time(state->start_seconds, 0);
                 // Total time in centiseconds

movement/watch_faces/complication/timer_face.c
@@ -45,8 +45,8 @@ static void _signal_callback() {
 
 static void _start(timer_state_t *state, bool with_beep) {
     if (state->timers[state->current_timer].value == 0) return;
-    watch_date_time_t now = watch_rtc_get_date_time();
-    state->now_ts = watch_utility_date_time_to_unix_time(now, movement_get_current_timezone_offset());
+    watch_date_time_t now = movement_get_utc_date_time();
+    state->now_ts = watch_utility_date_time_to_unix_time(now, 0);
     if (state->mode == pausing)
         state->target_ts = state->now_ts + state->paused_left;
     else
@@ -54,7 +54,7 @@ static void _start(timer_state_t *state, bool with_beep) {
                                                           state->timers[state->current_timer].unit.hours, 
                                                           state->timers[state->current_timer].unit.minutes, 
                                                           state->timers[state->current_timer].unit.seconds);
-    watch_date_time_t target_dt = watch_utility_date_time_from_unix_time(state->target_ts, movement_get_current_timezone_offset());
+    watch_date_time_t target_dt = watch_utility_date_time_from_unix_time(state->target_ts, 0);
     state->mode = running;
     movement_schedule_background_task_for_face(state->watch_face_index, target_dt);
     watch_set_indicator(WATCH_INDICATOR_BELL);
@@ -203,8 +203,8 @@ void timer_face_activate(void *context) {
     watch_display_string("TR", 0);
     watch_set_colon();
     if(state->mode == running) {
-        watch_date_time_t now = watch_rtc_get_date_time();
-        state->now_ts = watch_utility_date_time_to_unix_time(now, movement_get_current_timezone_offset());
+        watch_date_time_t now = movement_get_utc_date_time();
+        state->now_ts = watch_utility_date_time_to_unix_time(now, 0);
         watch_set_indicator(WATCH_INDICATOR_BELL);
     } else {
         state->pausing_seconds = 1;

movement/watch_faces/complication/tomato_face.c
@@ -42,7 +42,7 @@ static uint8_t get_length(tomato_state_t *state) {
 }
 
 static void tomato_start(tomato_state_t *state) {
-    watch_date_time_t now = watch_rtc_get_date_time();
+    watch_date_time_t now = movement_get_utc_date_time();
     int8_t length = (int8_t) get_length(state);
 
     state->mode = tomato_run;
@@ -120,8 +120,8 @@ void tomato_face_setup(uint8_t watch_face_index, void ** context_ptr) {
 void tomato_face_activate(void *context) {
     tomato_state_t *state = (tomato_state_t *)context;
     if (state->mode == tomato_run) {
-        watch_date_time_t now = watch_rtc_get_date_time();
-        state->now_ts = watch_utility_date_time_to_unix_time(now, movement_get_current_timezone_offset());
+        watch_date_time_t now = movement_get_utc_date_time();
+        state->now_ts = watch_utility_date_time_to_unix_time(now, 0);
         watch_set_indicator(WATCH_INDICATOR_BELL);
     }
     watch_set_colon();

movement/watch_faces/sensor/accelerometer_data_acquisition_face.c
@@ -437,8 +437,8 @@ static void start_reading(accelerometer_data_acquisition_state_t *state) {
     lis2dw_enable_fifo();
 
     accelerometer_data_acquisition_record_t record;
-    watch_date_time_t date_time = watch_rtc_get_date_time();
-    state->starting_timestamp = watch_utility_date_time_to_unix_time(date_time, movement_get_current_timezone_offset());
+    watch_date_time_t date_time = movement_get_utc_date_time();
+    state->starting_timestamp = watch_utility_date_time_to_unix_time(date_time, 0);
     record.header.info.record_type = ACCELEROMETER_DATA_ACQUISITION_HEADER;
     record.header.info.range = ACCELEROMETER_RANGE;
     record.header.info.temperature = lis2dw_get_temperature();

movement/watch_faces/settings/finetune_face.c
@@ -50,7 +50,7 @@ void finetune_face_activate(void *context) {
 }
 
 static float finetune_get_hours_passed(void) {
-    uint32_t current_time = watch_utility_date_time_to_unix_time(watch_rtc_get_date_time(), 0);
+    uint32_t current_time = watch_utility_date_time_to_unix_time(movement_get_utc_date_time(), 0);
     return (current_time - nanosec_state.last_correction_time) / 3600.0f;
 }
 
@@ -63,7 +63,7 @@ static void finetune_update_display(void) {
 
     if (finetune_page == 0) {
         watch_display_string("FT", 0);
-        watch_date_time_t date_time = watch_rtc_get_date_time();
+        watch_date_time_t date_time = movement_get_utc_date_time();
         sprintf(buf, "%02d", date_time.unit.second);
         watch_display_string(buf, 8);
 
@@ -104,7 +104,7 @@ static void finetune_adjust_subseconds(int delta) {
     watch_rtc_enable(false);
     delay_ms(delta);
     if (delta > 500) {
-        watch_date_time_t date_time = watch_rtc_get_date_time();
+        watch_date_time_t date_time = movement_get_local_date_time();
         date_time.unit.second = (date_time.unit.second + 1) % 60;
         if (date_time.unit.second == 0) { // Overflow
             date_time.unit.minute = (date_time.unit.minute + 1) % 60;
@@ -124,7 +124,7 @@ static void finetune_update_correction_time(void) {
     nanosec_state.freq_correction += roundf(nanosec_get_aging() * 100);
 
     // Remember when we last corrected time
-    nanosec_state.last_correction_time = watch_utility_date_time_to_unix_time(watch_rtc_get_date_time(), 0);
+    nanosec_state.last_correction_time = watch_utility_date_time_to_unix_time(movement_get_local_date_time(), 0);
     nanosec_save();
     movement_move_to_face(0); // Go to main face after saving settings
 }
@@ -144,7 +144,7 @@ bool finetune_face_loop(movement_event_t event, void *context) {
             // We flash green LED once per minute to measure clock error, when we are not on first screen
             if (finetune_page!=0) {
                 watch_date_time_t date_time;
-                date_time = watch_rtc_get_date_time();
+                date_time = movement_get_utc_date_time();
                 if (date_time.unit.second == 0) {
                     watch_set_led_green();
                     #ifndef __EMSCRIPTEN__

movement/watch_faces/settings/nanosec_face.c
@@ -45,7 +45,7 @@ const float voltage_coefficient = 0.241666667 * dithering; // 10 * ppm/V. Nomina
 static void nanosec_init_profile(void) {
     nanosec_changed = true;
     nanosec_state.correction_cadence = 10;
-    watch_date_time_t date_time = watch_rtc_get_date_time();
+    watch_date_time_t date_time = movement_get_utc_date_time();
     nanosec_state.last_correction_time = watch_utility_date_time_to_unix_time(date_time, 0);
 
     // init data after changing profile - do that once per profile selection
@@ -259,7 +259,7 @@ static void nanosec_next_edit_screen(void) {
 
 float nanosec_get_aging() // Returns aging correction in ppm
 {
-    watch_date_time_t date_time = watch_rtc_get_date_time();
+    watch_date_time_t date_time = movement_get_utc_date_time();
     float years = (watch_utility_date_time_to_unix_time(date_time, 0) - nanosec_state.last_correction_time) / 31536000.0f; // Years passed since finetune
     return years*nanosec_state.aging_ppm_pa/100.0f;
 }