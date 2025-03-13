5fa939786a39c3ba8181140442dbcdf767d92a65
Joey Castillo <joeycastillo@utexas.edu>
2025-03-12 23:15:24 -0400

    fix warning in upstream musl function, see https://git.musl-libc.org/cgit/musl/commit/src/time/__year_to_secs.c\?id\=2d84486a08c57d00b647586b7cbe2dcec2728e81

--- watch-library/shared/watch/watch_utility.c
+++ watch-library/shared/watch/watch_utility.c
@@ -123,9 +123,9 @@ static uint32_t __year_to_secs(uint32_t year, int *is_leap)
 		return 31536000*(y-70) + 86400*leaps;
 	}
 
-	int cycles, centuries, leaps, rem;
+	int cycles, centuries, leaps, rem, dummy;
 
-	if (!is_leap) is_leap = &(int){0};
+	if (!is_leap) is_leap = &dummy;
 	cycles = (year-100) / 400;
 	rem = (year-100) % 400;
 	if (rem < 0) {
