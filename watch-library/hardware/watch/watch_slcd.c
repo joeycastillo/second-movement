5059582e96bbd1734e4b3678fb2bac9b0c0b7bcd
Joey Castillo <joeycastillo@utexas.edu>
2025-05-11 20:16:40 -0400

    improved contrast on custom LCD

--- watch-library/hardware/watch/watch_slcd.c
+++ watch-library/hardware/watch/watch_slcd.c
@@ -197,7 +197,7 @@ void watch_enable_display(void) {
     slcd_clear();
 
     if (_installed_display == WATCH_LCD_TYPE_CUSTOM) {
-        slcd_set_contrast(6);
+        slcd_set_contrast(4);
     } else {
         slcd_set_contrast(9);
     }
