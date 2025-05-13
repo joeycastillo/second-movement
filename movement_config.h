3f9662db59f41d7386f7e874715017d82b0c62f4
Joey Castillo <joeycastillo@utexas.edu>
2025-05-13 18:23:03 -0400

    new watch face order for pro

--- movement_config.h
+++ movement_config.h
@@ -34,12 +34,10 @@ const watch_face_t watch_faces[] = {
     moon_phase_face,
     countdown_face,
     stopwatch_face,
-    activity_logging_face,
-    voltage_face,
-    days_since_face,
-    accel_interrupt_count_face,
     settings_face,
     set_time_face,
+    temperature_display_face,
+    voltage_face
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -50,7 +48,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 5)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
