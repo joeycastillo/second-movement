02c567770da06ad9410744b47176604c8131ec79
Joey Castillo <joeycastillo@utexas.edu>
2025-05-15 09:07:33 -0400

    no temperature face in defaults

--- movement_config.h
+++ movement_config.h
@@ -34,7 +34,6 @@ const watch_face_t watch_faces[] = {
     moon_phase_face,
     countdown_face,
     stopwatch_face,
-    temperature_display_face,
     voltage_face,
     settings_face,
     set_time_face
@@ -48,7 +47,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 3)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
