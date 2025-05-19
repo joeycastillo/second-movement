4a04db6ca66bbfe5c0f943a1aa25ef65e4ab48fd
Joey Castillo <joeycastillo@utexas.edu>
2025-05-18 20:45:37 -0400

    light meter: was written today lol

--- watch-faces/sensor/light_meter_face.c
+++ watch-faces/sensor/light_meter_face.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2024 Joey Castillo
+ * Copyright (c) 2025 Joey Castillo
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
