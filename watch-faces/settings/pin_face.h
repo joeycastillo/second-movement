d2fd77f55042ea86d8adeb42aa4c858dfd5528a9
Alessandro Genova <ales.genova@gmail.com>
2025-08-02 13:40:36 -0400

    Add documentation for the pin_service and pin_face

--- watch-faces/settings/pin_face.h
+++ watch-faces/settings/pin_face.h
@@ -24,6 +24,29 @@
 
 #pragma once
 
+/*
+ * PIN face
+ *
+ * Adding the PIN face to a build will automatically enable and lock the PIN service.
+ * In the PIN face UI the user will be able to:
+ * - Lock/Unlock the PIN service by entering the current PIN
+ * - Change the current PIN by entering the OLD PIN, the NEW PIN, and confirming the NEW PIN again.
+ * - Configure when the PIN service will be automatically locked after successfully unlocking (5min default).
+ * 
+ * A PIN is a 6 digit sequence. When entering a PIN through this face, each button press will correspond to a new digit.
+ * The encoding from button press to numerical value is the following:
+ * MODE_DOWN   ->  0
+ * MODE_LONG   ->  1
+ * LIGHT_DOWN  ->  2
+ * LIGHT_LONG  ->  3
+ * ALARM_DOWN  ->  4
+ * ALARM_LONG  ->  5
+ * 
+ * Navigation:
+ * - In the menus use the LIGHT button to advance and the ALARM button to select the option.
+ * - After a failed PIN attempt press the ALARM button to try again, and the LIGHT button to go back to main menu.
+ */
+
 #include "movement.h"
 
 void pin_face_setup(uint8_t watch_face_index, void ** context_ptr);
