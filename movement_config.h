a34a266903303fa4afaeb07c98c13396b7501b39
Alessandro Genova <ales.genova@gmail.com>
2025-08-12 22:25:47 -0400

    Add a face to debug some counter32 metrics

--- movement_config.h
+++ movement_config.h
@@ -38,7 +38,8 @@ const watch_face_t watch_faces[] = {
     temperature_display_face,
     voltage_face,
     settings_face,
-    set_time_face
+    set_time_face,
+    rtccount_face,
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -49,7 +50,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 5)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
