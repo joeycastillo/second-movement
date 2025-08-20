95df9a66838ae2ae3dd6dc492a0bd3ffb05a18ad
David Volovskiy <devolov@gmail.com>
2025-08-19 20:16:43 -0400

    Enable tap functionality

--- watch-faces/complication/endless_runner_face.c
+++ watch-faces/complication/endless_runner_face.c
@@ -525,6 +525,7 @@ void endless_runner_face_activate(void *context) {
     ball_arr_seg = is_custom_lcd ? custom_ball_arr_seg : classic_ball_arr_seg;
     obstacle_arr_com = is_custom_lcd ? custom_obstacle_arr_com : classic_obstacle_arr_com;
     obstacle_arr_seg = is_custom_lcd ? custom_obstacle_arr_seg : classic_obstacle_arr_seg;
+    movement_enable_tap_detection_if_available();
 }
 
 bool endless_runner_face_loop(movement_event_t event, void *context) {
@@ -588,5 +589,6 @@ bool endless_runner_face_loop(movement_event_t event, void *context) {
 
 void endless_runner_face_resign(void *context) {
     (void) context;
+    movement_disable_tap_detection_if_available();
 }
 
