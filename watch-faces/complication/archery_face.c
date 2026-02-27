ce454a33d02acd4b58df9f13d6c8f8f944a246ff
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2026-02-27 21:10:09 +0100

    Add author information

--- watch-faces/complication/archery_face.c
+++ watch-faces/complication/archery_face.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2026 <#author_name#>
+ * Copyright (c) 2026 Davide Girardi
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
