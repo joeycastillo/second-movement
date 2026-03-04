eba8b7953e976b3d01f9ec9f69b7686833d669dc
Raffael Mancini <raffael.mancini@mnhn.lu>
2026-03-04 14:24:27 +0100

    Revert more config

--- movement_config.h
+++ movement_config.h
@@ -50,7 +50,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX 5
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 5)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
@@ -107,6 +107,6 @@ const watch_face_t watch_faces[] = {
  * A value of 4 is a good starting point if you have issues
  * with multiple button presses firing.
 */
-#define MOVEMENT_DEBOUNCE_TICKS 4
+#define MOVEMENT_DEBOUNCE_TICKS 0
 
 #endif // MOVEMENT_CONFIG_H_
